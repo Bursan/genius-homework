d3235c57a49a3227e20e8dce689cac8c79019f58
Александр Бурдин <blindmas7er89@gmail.com>
2024-07-05 11:39:36 +0300

    fix hw3 last swiper media and retina

--- homework-4/js/slider.js
+++ homework-4/js/slider.js
@@ -1,34 +1,22 @@
-var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination"
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    // slidesPerView: 3,
-    // spaceBetween: 25,
-    loop: true,
-  
-    // Responsive breakpoints
-    breakpoints: {
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      },
-      // when window width is >= 640px
-      756: {
-        slidesPerView: 2,
-        spaceBetween: 30
-      },
-      991: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 25
-      }
-    }
-  });
-  
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 16,
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 2,
+  //   },
+  //   1280: {
+  //     slidesPerView: 3,
+  //   },
+  // },
+  pagination: {
+    el: ".pagination",
+    bulletClass: "pagination__button",
+    bulletActiveClass: "pagination__button--active",
+  },
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
