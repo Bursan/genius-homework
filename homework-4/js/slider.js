88d2f55fc52d65afb134446ecc9173d748394355
Александр Бурдин <blindmas7er89@gmail.com>
2024-07-05 21:55:54 +0300

    try fix flex container swiper (min-width)

--- homework-4/js/slider.js
+++ homework-4/js/slider.js
@@ -2,14 +2,14 @@ const swiper = new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 3,
   spaceBetween: 16,
-  // breakpoints: {
-  //   768: {
-  //     slidesPerView: 2,
-  //   },
-  //   1280: {
-  //     slidesPerView: 3,
-  //   },
-  // },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
   pagination: {
     el: ".pagination",
     bulletClass: "pagination__button",
