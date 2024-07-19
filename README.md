5eddad75a11b39f142444d2010ac2437960b9415
Александр Бурдин <blindmas7er89@gmail.com>
2024-07-19 04:18:36 +0300

    last w3c retina slider check bem

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 [Homework 2](https://bursan.github.io/genius-homework/homework-2/)<br>
 [Homework 3](https://bursan.github.io/genius-homework/homework-3/)<br>
 [Homework 4](https://bursan.github.io/genius-homework/homework-4/)<br>
-Final Project (to be...)
+Final Project (to be...)js
+[JS 1](https://bursan.github.io/genius-homework/hw-js-1/js)<br>
+[JS 2](https://bursan.github.io/genius-homework/hw-js-2/js)<br>
+[JS 3](https://bursan.github.io/genius-homework/hw-js-3/js)<br>
+[JS 4](https://bursan.github.io/genius-homework/hw-js-4/js)<br>
+[JS 5](https://bursan.github.io/genius-homework/hw-js-5/js)<br>
+[JS 6](https://bursan.github.io/genius-homework/hw-js-6/js)<br>
+[JS 7](https://bursan.github.io/genius-homework/hw-js-7/js)<br>
+[JS 8](https://bursan.github.io/genius-homework/hw-js-8/js)<br>
+[JS 9](https://bursan.github.io/genius-homework/hw-js-9/js)<br>
+[JS 10](https://bursan.github.io/genius-homework/hw-js-10/js)<br>
+[JS 11](https://bursan.github.io/genius-homework/hw-js-11/js)<br>
+[JS 12](https://bursan.github.io/genius-homework/hw-js-12/js)<br>
+[JS 13](https://bursan.github.io/genius-homework/hw-js-13/js)<br>
--- homework-4/js/mobile-menu.js
+++ homework-4/js/mobile-menu.js
@@ -1,13 +1,19 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
+const menuLnkClose = document.querySelectorAll(".menu-lnk-close");
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-const disableScroll = () =>
-  document.body.classList.toggle('is-scroll-disabled');
+for (var i = 0; i < menuLnkClose.length; i++) {
+  const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+  menuLnkClose[i].addEventListener("click", toggleMenu);
+}
 
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+// const disableScroll = () =>
+//   document.body.classList.toggle("is-scroll-disabled");
 
-menuBtnOpen.addEventListener('click', disableScroll);
-menuBtnClose.addEventListener('click', disableScroll);
+menuBtnOpen.addEventListener("click", toggleMenu);
+menuBtnClose.addEventListener("click", toggleMenu);
+
+// menuBtnOpen.addEventListener("click", disableScroll);
+// menuBtnClose.addEventListener("click", disableScroll);
