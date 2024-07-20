ade5c3a4eb5f2d22dc4e72d6eaee0a895fa04176
Александр Бурдин <blindmas7er89@gmail.com>
2024-07-20 22:28:11 +0300

    redo slider, done

--- homework-4/js/slider.js
+++ homework-4/js/slider.js
@@ -1,5 +1,8 @@
-const swiper = new Swiper(".swiper-container", {
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
   loop: true,
+
   slidesPerView: 1,
   spaceBetween: 16,
   breakpoints: {
@@ -10,13 +13,44 @@ const swiper = new Swiper(".swiper-container", {
       slidesPerView: 3,
     },
   },
+
+  // If we need pagination
   pagination: {
-    el: ".pagination",
-    bulletClass: "pagination__button",
-    bulletActiveClass: "pagination__button--active",
+    el: ".swiper-pagination",
+    clickable: true,
   },
+
+  // Navigation arrows
   navigation: {
-    nextEl: ".carousel-button.next",
-    prevEl: ".carousel-button.prev",
+    nextEl: ".slider__right_arrow",
+    prevEl: ".slider__left_arrow",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
   },
 });
+
+// const swiper = new Swiper(".swiper-container", {
+//   loop: true,
+//   slidesPerView: 1,
+//   spaceBetween: 16,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1280: {
+//       slidesPerView: 3,
+//     },
+//   },
+//   pagination: {
+//     el: ".pagination",
+//     bulletClass: "pagination__button",
+//     bulletActiveClass: "pagination__button--active",
+//   },
+//   navigation: {
+//     nextEl: ".carousel-button.next",
+//     prevEl: ".carousel-button.prev",
+//   },
+// });
